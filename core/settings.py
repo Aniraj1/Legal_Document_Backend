ca40d97d5f4e33f04bec266dd16452f5bf408e73
aniraj1 <anirajshahi@gmail.com>
2026-03-27 01:18:11 +1030

    Setting changes

--- core/settings.py
+++ core/settings.py
@@ -83,8 +83,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config("DB_ENGINE", default='django.db.backends.sqlite3'),
+        'NAME': config("DB_NAME", default=BASE_DIR / 'db.sqlite3'),
     }
 }
 
